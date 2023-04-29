d8f3a88f12453ef19c8393852cb880f08b108c6a
Mahir Patel <126451249+mahirpatel04@users.noreply.github.com>
2023-04-29 14:49:42 -0700

    Return Button Added

--- main.py
+++ main.py
@@ -21,44 +21,110 @@ introFrame = customtkinter.CTkFrame(root)
 label = customtkinter.CTkLabel(master=introFrame, text="WASTE MANAGEMENT APP", fg_color=('white' ,"green"), corner_radius=8, height=60, width=180, font=("Helvetica", 24) )
 label.place(relx=0.5, rely=0.1, anchor=customtkinter.CENTER)
 
-userWaste = ""
 
+userWaste = ""
+currentFrame = introFrame
 
 
 # BUTTON/ENTER EVENT
-def button_event():
+def originalButton():
     global entry
     userWaste = entry.get()
-    print(userWaste)
     
     # if statements to check for types of trash
-    if "battery" in userWaste:
-        battery()
-            
+    if "recycle" in userWaste.lower():
+        recycle()
+        currentFrame = recycleFrame
+    elif "trash" in userWaste.lower():
+        trash()
+        currentFrame = trashFrame
+    elif "compost" in userWaste.lower():
+        compost()
+        currentFrame = compostFrame
+    else:
+        notAvailable()
+        currentFrame = notAvailableFrame
+
+  
     
+
 # ENTRY
 entry = customtkinter.CTkEntry(master=introFrame, width=200, height=50, corner_radius=10)
-entry.place(relx=0.5, rely=0.4, anchor=customtkinter.CENTER)
+entry.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
+
 
 
 # BUTTON
-button = customtkinter.CTkButton(master=introFrame, text="Classify My Waste", command=button_event, width=120, 
+button = customtkinter.CTkButton(master=introFrame, text="Classify My Waste", command=originalButton, width=120, 
                                  height=32, border_width=0, corner_radius=8)
-button.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
-entry.bind('<Return>', lambda event: button_event())
+button.place(relx=0.5, rely=0.8, anchor=customtkinter.CENTER)
+entry.bind('<Return>', lambda event: originalButton())
+
+def returnButton(currentFrame):
+    returnButton = customtkinter.CTkButton(master=currentFrame, text="Return to home", command=lambda: leaveButton(currentFrame), width=120,
+                                           height=32, border_width=0, corner_radius=8)
+    returnButton.place(relx=0.5, rely=0.8, anchor=customtkinter.CENTER)
+
 
 #different frames depending on the type of waste
-batteryFrame = customtkinter.CTkFrame(root)
-battery_label = tk.Label(batteryFrame, text="Battery Info Goes Here")
+recycleFrame = customtkinter.CTkFrame(root)
+recycleLabel = customtkinter.CTkLabel(recycleFrame, text="Recylce Info Goes Here")
+recycleLabel.place(relx=0.5, rely=0.1, anchor=customtkinter.CENTER)
+returnButton(recycleFrame)
 
-glassFrame = customtkinter.CTkFrame(root)
+trashFrame = customtkinter.CTkFrame(root)
+trashLabel = customtkinter.CTkLabel(trashFrame, text="Trash Info Goes Here")
+trashLabel.place(relx=0.5, rely=0.1, anchor=customtkinter.CENTER)
+returnButton(trashFrame)
+
+compostFrame = customtkinter.CTkFrame(root)
+compostLabel = customtkinter.CTkLabel(compostFrame, text="Compost Info Goes Here")
+compostLabel.place(relx=0.5, rely=0.1, anchor=customtkinter.CENTER)
+returnButton(compostFrame)
+
+notAvailableFrame = customtkinter.CTkFrame(root)
+notAvailableLabel = customtkinter.CTkLabel(notAvailableFrame, text="ur a retard just shove the trash up ur ass")
+notAvailableLabel.place(relx=0.5, rely=0.1, anchor=customtkinter.CENTER)
+returnButton(notAvailableFrame)
+
 
 #different types of wastes and their pages
 
-def battery():
+def recycle():
     introFrame.pack_forget()
-    batteryFrame.pack()
+    currentFrame = recycleFrame
+    currentFrame.pack()
     
+    
+def trash():
+    introFrame.pack_forget()
+    currentFrame = trashFrame
+    currentFrame.pack()
+
+def compost():
+    introFrame.pack_forget()
+    currentFrame = compostFrame
+    compostFrame.pack()
+
+def notAvailable():
+    introFrame.pack_forget()
+    currentFrame = notAvailableFrame
+    currentFrame.pack()
+    
+def leaveButton(currentFrame):
+    currentFrame.pack_forget()
+    introFrame.pack()
+
+
+
+
+introFrame.pack()
+
+
+# Run the main loop to start the app
+root.mainloop()
+
+
 
 
 
