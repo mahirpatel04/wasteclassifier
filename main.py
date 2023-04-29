a802d332cb249d3a9ded5b5663ed1637b4de27d6
Mahir Patel <126451249+mahirpatel04@users.noreply.github.com>
2023-04-29 11:53:03 -0700

    Framework
    Added different frames for the intro and other different types of waste. Not completely functional yet.

--- main.py
+++ main.py
@@ -14,11 +14,16 @@ width = "500"
 height = "570"
 root.geometry(width + "x" + height)
 
-# Adds a label to the window
-label = tk.Label(root, text="Hello, World!")
+#Creates the introFrame
+introFrame = customtkinter.CTkFrame(root)
+
+# Adds a label to the frame
+label = tk.Label(introFrame, text="Hello, World!")
 label.pack()
 
 userWaste = ""
+
+
 
 # BUTTON/ENTER EVENT
 def button_event():
@@ -28,26 +33,38 @@ def button_event():
     
     # if statements to check for types of trash
     if "battery" in userWaste:
-        print("you likely have battery in ur waste")
+        battery()
+            
     
 # ENTRY
-entry = customtkinter.CTkEntry(master=root, width=200, height=50, corner_radius=10)
+entry = customtkinter.CTkEntry(master=introFrame, width=200, height=50, corner_radius=10)
 entry.place(relx=0.5, rely=0.4, anchor=customtkinter.CENTER)
 
 
 # BUTTON
-button = customtkinter.CTkButton(master=root, text="Classify My Waste", command=button_event, width=120, 
+button = customtkinter.CTkButton(master=introFrame, text="Classify My Waste", command=button_event, width=120, 
                                  height=32, border_width=0, corner_radius=8)
 button.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
 entry.bind('<Return>', lambda event: button_event())
 
+#different frames depending on the type of waste
+batteryFrame = customtkinter.CTkFrame(root)
+battery_label = tk.Label(batteryFrame, text="Battery Info Goes Here")
+
+glassFrame = customtkinter.CTkFrame(root)
+
+#different types of wastes and their pages
+
+def battery():
+    introFrame.pack_forget()
+    batteryFrame.pack()
+    
 
 
 
 
 
-
-
+introFrame.pack()
 
 
 # Run the main loop to start the app
